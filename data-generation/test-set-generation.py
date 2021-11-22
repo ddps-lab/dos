9d10a6c9b610793290d2b6ab5519b1acdd1d628c
unhochoi <yms04089@kookmin.ac.kr>
2021-11-22 21:24:13 +0900

    Update test-set-generation.py

--- data-generation/test-set-generation.py
+++ data-generation/test-set-generation.py
@@ -11,16 +11,23 @@ dataset = pd.read_csv("../data/spmm-data.csv")
 # Row number of train set extracted with DOE
 train_set = []
 
+train_row_start = False
+line_number = 0
 # Input from pipeline
 for line in sys.stdin:
-
-    # Set up regular expression compilation
-    rule = re.compile('[0-9]+')
-    # Apply regular expression to line
-    temp = rule.findall(line)
-    # Collect rows of train set
-    for row in temp[1:]:
-        train_set.append(int(row)-1)
+    
+    line_number += 1
+    
+    if (line_number == 5):
+        train_row_start = True
+    if (train_row_start == True):
+        # Set up regular expression compilation
+        rule = re.compile('[0-9]+')
+        # Apply regular expression to line
+        temp = rule.findall(line)
+        # Collect rows of train set
+        for row in temp[1:]:
+            train_set.append(int(row)-1)
 
 # Generate data that is not extracted as train set from the entire dataset as test set
 test_set = dataset.drop(index=train_set, axis=0)
